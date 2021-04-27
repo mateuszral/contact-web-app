9bc6524aeacadb3d6b1c407682c59a8a6d62c5e6
Mateusz Ral <mateuszral@onet.pl>
2021-04-27 20:06:23 +0200

    Add icon to input

--- src/components/molecules/FormField/FormField.js
+++ src/components/molecules/FormField/FormField.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
 
 import Input from 'components/atoms/Input/Input';
 import Label from 'components/atoms/Label/Label';
@@ -9,27 +10,36 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  position: relative;
+
+  svg {
+    position: absolute;
+    right: 10px;
+    top: 52px;
+    color: ${({ theme }) => theme.warning};
+  }
 `;
 
-const FormField = ({ onChange, label, value, name, id, placeholder, type = 'text', ...props }) => (
-  <Wrapper>
-    <Label htmlFor={id}>{label}</Label>
-    <Input
-      name={name}
-      id={id}
-      type={type}
-      value={value}
-      placeholder={placeholder}
-      onChange={onChange}
-      {...props}
-    />
-  </Wrapper>
+const FormField = forwardRef(
+  ({ label, name, id, placeholder, type = 'text', isError, ...props }, ref) => (
+    <Wrapper>
+      <Label htmlFor={id}>{label}</Label>
+      <Input
+        name={name}
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        ref={ref}
+        isError={isError}
+        {...props}
+      />
+      {isError && <AiOutlineExclamationCircle />}
+    </Wrapper>
+  ),
 );
 
 FormField.propTypes = {
-  onChange: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
