601400bae3900450ce61cce2b33e4890b1df46a9
Mateusz Ral <mateuszral@onet.pl>
2021-04-27 20:04:41 +0200

    Add header story

---
+++ src/components/molecules/Header/Header.stories.js
@@ -0,0 +1,10 @@
+import Header from './Header';
+
+export default {
+  title: 'Components/Molecules/Header',
+  component: Header,
+};
+
+const Template = (args) => <Header {...args} />;
+
+export const Default = Template.bind({});
